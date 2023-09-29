fix so3 rotation function

examples/equivariance_plot/main.py
@@ -11,10 +11,16 @@ from s2cnn import s2_near_identity_grid, so3_near_identity_grid
 
 
 def s2_rotation(x, a, b, c):
-    x = tf.expand_dims(x, -1)  # Add a new last dimension
-    shape = tf.concat([tf.shape(x)[:-1], [tf.shape(x)[-2]]], 0)  # Create a shape for tiling
-    x = tf.tile(x, shape)  # Tile the tensor along the new last dimension
+    shape = tf.shape(x)
 
+    # Add a new dimension to get a shape similar to [1, 3, 128, 128, 1]
+    x = tf.expand_dims(x, axis=-1)
+
+    # Create the multiples dynamically
+    multiples = tf.concat([tf.ones_like(shape, dtype=tf.int32), [shape[-1]]], axis=0)
+
+    # Tile the tensor to get the final expanded shape
+    x = tf.tile(x, multiples)
     x = so3_rotation(x, a, b, c) 
     return x[..., 0]
 

s2cnn/soft/so3_rotation.py
@@ -1,5 +1,5 @@
 # pylint: disable=C,R,E1101
-import torch
+import tensorflow as tf
 import numpy as np
 
 from .so3_fft import SO3_fft_real, SO3_ifft_real
@@ -12,13 +12,13 @@ def so3_rotation(x, alpha, beta, gamma):
     '''
     :param x: [..., beta, alpha, gamma] (..., 2b, 2b, 2b)
     '''
-    b = x.size()[-1] // 2
-    x_size = x.size()
+    b = x.shape[-1] // 2
+    x_size = x.shape.as_list()
 
-    Us = _setup_so3_rotation(b, alpha, beta, gamma, device_type=x.device.type, device_index=x.device.index)
+    Us = _setup_so3_rotation(b, alpha, beta, gamma)
 
     # fourier transform
-    x = SO3_fft_real.apply(x)  # [l * m * n, ..., complex]
+    x = SO3_fft_real(x, b_out=None)  # [l * m * n, ..., complex]
 
     # rotated spectrum
     Fz_list = []
@@ -28,22 +28,21 @@ def so3_rotation(x, alpha, beta, gamma):
         size = L ** 2
 
         Fx = x[begin:begin+size]
-        Fx = Fx.view(L, -1, 2)  # [m, n * batch, complex]
+        Fx = tf.reshape(Fx, (L, -1, 2))  # [m, n * batch, complex]
 
-        U = Us[l].view(L, L, 2)  # [m, n, complex]
+        U = tf.reshape(Us[l], (L, L, 2))  # [m, n, complex]
 
         Fz = complex_mm(U, Fx, conj_x=True)  # [m, n * batch, complex]
 
-        Fz = Fz.view(size, -1, 2)  # [m * n, batch, complex]
+        Fz = tf.reshape(Fz, (size, -1, 2))  # [m * n, batch, complex]
         Fz_list.append(Fz)
 
         begin += size
 
-    Fz = torch.cat(Fz_list, 0)  # [l * m * n, batch, complex]
-    z = SO3_ifft_real.apply(Fz)
+    Fz = tf.concat(Fz_list, axis=0)  # [l * m * n, batch, complex]
+    z = SO3_ifft_real(Fz, b_out=None)
 
-    z = z.contiguous()
-    z = z.view(*x_size)
+    z = tf.reshape(z, x_size)
 
     return z
 
@@ -63,10 +62,10 @@ def __setup_so3_rotation(b, alpha, beta, gamma):
 
 
 @lru_cache(maxsize=32)
-def _setup_so3_rotation(b, alpha, beta, gamma, device_type, device_index):
+def _setup_so3_rotation(b, alpha, beta, gamma):
     Us = __setup_so3_rotation(b, alpha, beta, gamma)
 
-    # convert to torch Tensor
-    Us = [torch.tensor(U, dtype=torch.float32, device=torch.device(device_type, device_index)) for U in Us]  # pylint: disable=E1102
+    # convert to tensorflow Tensor
+    Us = [tf.convert_to_tensor(U, dtype=tf.float32) for U in Us]  # pylint: disable=E1102
 
     return Us